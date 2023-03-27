9adbc09a7fc9887ac9155d0767f6925563b635bd
LuizFelipe90 <Luuiiz-Felipe@hotmail.com>
2023-03-27 15:40:19 -0400

    adicionando os primeiros arquivos

---
+++ src/main/java/com/mycompany/projetoaula/ProjetoAula.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetoaula;
+
+/**
+ *
+ * @author usuario
+ */
+public class ProjetoAula {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
